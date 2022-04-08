a58694661c832eb32f158e084dc47ec8b9c0b520
Isaac Palou <isaacpalou@paucasesnovescifp.cat>
2022-04-08 20:46:41 +0200

    first commit

---
+++ src/cat/paucasesnoves/isaac/Ejercici1.java
@@ -0,0 +1,18 @@
+package cat.paucasesnoves.isaac;
+
+public class Ejercici1 {
+	/** Exercici 1
+
+	* Calcula la longitud d'una circumferència a partir del radi.
+
+	* @author Isaac
+
+	*/
+	public static void main(String[] args) {
+		double pi = Math.PI;
+		double radio = 45;
+		double longitud;
+		longitud = 2 * pi * radio;
+		System.out.println( "Longitud: " + longitud);
+	}
+}
---
+++ src/cat/paucasesnoves/isaac/Ejercici2.java
@@ -0,0 +1,18 @@
+package cat.paucasesnoves.isaac;
+
+public class Ejercici2 {
+	/** Exercici 2
+
+	* Donada una velocitat v expressada en km/h passar-la a m/s (v*1000/3600).
+
+	* Feis el càlcul amb decimals.
+
+	* @author Isaac
+
+	*/
+	public static void main(String[] args) {
+		
+		
+	}
+
+}
