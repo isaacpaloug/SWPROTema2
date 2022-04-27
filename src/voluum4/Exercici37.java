6b857023aae29d2223f81db308519759a2c31123
Isaac Palou <isaacpalou@paucasesnovescifp.cat>
2022-04-27 15:22:14 +0200

    volum4

--- src/voluum4/Exercici37.java
+++ src/voluum4/Exercici37.java
@@ -2,27 +2,24 @@ package voluum4;
 
 public class Exercici37 {
 	public static void main(String[] args) {
-		int[][] m = new int[4][4];
-        /*1.Inicialitzau-la de forma que cada casella tengui la suma de les seves 
-         coordenades.*/
-        for (int i = 0; i < m.length; i++) {
-            for (int j = 0; j < m[i].length; j++) {
-                m[i][j] = i + j;
-                System.out.print(m[i][j] + "\t");
+		int[][] matriu = new int[4][4];
+		
+        for (int i = 0; i < matriu.length; i++) {
+            for (int j = 0; j < matriu[i].length; j++) {
+                matriu[i][j] = i + j;
+                System.out.print(matriu[i][j] + "\t");
             }
             System.out.println();
         }
-        /*2. Mostrau la diagonal principal, [1,1], [2,2]... */
-        System.out.println("Diagonal principal");
-        for (int i = 0; i < m.length; i++) {
-            System.out.print(m[i][i] + "\t");
+        System.out.println("Principal");
+        for (int i = 0; i < matriu.length; i++) {
+            System.out.print(matriu[i][i] + "\t");
         }
         System.out.println();
 
-        /*3. Mostrau la diagonal secundària [4,1][3,2]...*/
-        System.out.println("Diagonal secundaria");
-        for (int i = 0, j=m.length-1; i < m.length; i++, j--) {
-            System.out.print(m[i][j] + "\t");
+        System.out.println("Secundaria");
+        for (int i = 0, j=matriu.length-1; i < matriu.length; i++, j--) {
+            System.out.print(matriu[i][j] + "\t");
         }
         System.out.println();
 	}
